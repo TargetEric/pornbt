3039dbd21771f7f1210218874fa385e113270621
TargetEric <nkansara1122@gmail.com>
2024-09-24 12:56:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7546434205:AAGN7aIVMB8VI63eU_udN6PMB7nLzyllluw")
+API_ID = int(os.environ.get("API_ID", "6131334"))
+API_HASH = os.environ.get("API_HASH", "9514b24d20d23c06bae8a5c2b60fa0c7")
 SPOILER_MODE = bool(os.environ.get("SPOILER_MODE", True))
